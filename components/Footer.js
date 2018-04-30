7b5b911c1538ada0406bec1a49a39b964236d734
Trevor <litseyt@me.com>
2018-04-30 11:55:52 -0500

    export script

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,12 @@ const Footer = () => {
 	return (
 		<div>
 			<p>Analyze any YouTube playlist or channel. Get total views, duration, likes, dislikes and more.</p>
+			<a href="https://github.com/trevorlitsey/youtube-analyzer" target="blank">GitHub</a>
 			<style jsx>{`
+
 				text-align: center;
 				padding: 30px;
+
 		`}</style>
 		</div>
 	)
