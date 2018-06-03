e98bc780f1d5f1f240d4f2debf4e6808a47ac3eb
Trevor <litseyt@me.com>
2018-06-03 18:34:34 -0500

    more lenient ID check

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "0b38796470cc70408dab"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "302de44d00e8e8dc915c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js","4":"bundles/pages/search.js"}[chunkId]||chunkId) + "-" + {"0":"3071c82fc573461ebffd","1":"3763de13a4559be52569","3":"bda4c61c411a2c66f16c","4":"dc9f38da53fc53499de3"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js","4":"bundles/pages/search.js"}[chunkId]||chunkId) + "-" + {"0":"3071c82fc573461ebffd","1":"3763de13a4559be52569","3":"ba3d6b2cee729e1b796a","4":"dc9f38da53fc53499de3"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- components/Form.js
+++ components/Form.js
@@ -1,114 +1,117 @@
 // @flow
 
-import React from 'react';
-import propTypes from 'prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faSync from '@fortawesome/fontawesome-free-solid/faSync'
-import { isURL, isEmpty } from 'validator';
+import React from "react";
+import propTypes from "prop-types";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faSync from "@fortawesome/fontawesome-free-solid/faSync";
+import { isURL, isEmpty } from "validator";
 
-import { getPlaceHolderTest } from './helpers';
+import { getPlaceHolderTest } from "./helpers";
 
 type Props = {
-	handleSubmit: any,
-}
+  handleSubmit: any,
+};
 
 type State = {
-	loading: boolean,
-	placeholder: string,
-	errorMessage: string,
-}
+  loading: boolean,
+  placeholder: string,
+  errorMessage: string,
+};
 
 class Form extends React.PureComponent<Props, State> {
+  state = {
+    loading: false,
+    placeholder: getPlaceHolderTest("playlistUrl"),
+    errorMessage: "",
+  };
 
-	state = {
-		loading: false,
-		placeholder: getPlaceHolderTest('playlistUrl'),
-		errorMessage: '',
-	}
+  updatePlaceholder = () => {
+    const placeholder = getPlaceHolderTest(
+      this.refs.option && this.refs.option.value,
+    );
+    this.setState({ placeholder });
+  };
 
-	updatePlaceholder = () => {
-		const placeholder = getPlaceHolderTest(this.refs.option && this.refs.option.value)
-		this.setState({ placeholder });
-	}
+  handleSubmit = (e: SyntheticEvent<HTMLButtonElement>) => {
+    e.preventDefault();
 
-	handleSubmit = (e: SyntheticEvent<HTMLButtonElement>) => {
-		e.preventDefault();
+    const option = this.refs.option.value;
+    const text = this.refs.text.value;
 
-		const option = this.refs.option.value
-		const text = this.refs.text.value
+    // is empty
+    if (isEmpty(text)) {
+      const thing = option.includes("Url") ? "a url" : "an id";
+      return this.setState({ errorMessage: `Please enter ${thing}` });
+    }
 
-		// is empty
-		if (isEmpty(text)) {
-			const thing = option.includes('Url') ? 'a url' : 'an id'
-			return this.setState({ errorMessage: `Please enter ${thing}` })
-		}
+    // is url
+    if (option.includes("Url") && !isURL(text)) {
+      return this.setState({ errorMessage: "Please enter a valid url!" });
+    }
 
-		// is url
-		if (option.includes('Url') && !isURL(text)) {
-			return this.setState({ errorMessage: 'Please enter a valid url!' })
-		}
+    // is id
+    else if (option.includes("Id") && isURL(text)) {
+      return this.setState({ errorMessage: "Please enter a valid ID!" });
+    }
 
-		// is id
-		else if (option.includes('Id') && /[^A-Za-z\d\-]/.test(text)) {
-			return this.setState({ errorMessage: 'Ids should only contain letters, numbers and dashes' })
-		}
+    // all good
+    this.props.handleSubmit({ option, text });
+    this.setState({ loading: true });
+  };
 
-		// all good
-		this.props.handleSubmit({ option, text })
-		this.setState({ loading: true });
+  render() {
+    const { loading, errorMessage, placeholder } = this.state;
 
-	}
+    const loaderIcon = (
+      <FontAwesomeIcon icon={faSync} spin style={{ marginLeft: 10 }} />
+    );
 
-	render() {
+    return (
+      <div className="wrapper">
+        <form onSubmit={this.handleSubmit}>
+          <h3 className="title">YouTube Analyzer</h3>
+          <select onChange={this.updatePlaceholder} ref="option" name="options">
+            <option value="playlistUrl">Playlist URL</option>
+            <option value="playlistId">Playlist ID</option>
+            <option value="channelUrl">Channel URL</option>
+            <option value="channelId">Channel ID</option>
+          </select>
+          <div>
+            <input type="text" ref="text" placeholder={placeholder} />
+            <span className="form-error is-visible" id="uuid">
+              {errorMessage}
+            </span>
+          </div>
+          <button className="primary button" type="submit">
+            Submit {loading && loaderIcon}
+          </button>
+        </form>
+        <style jsx>{`
+          .wrapper {
+            padding: 30px 20px;
+            max-width: 700px;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            margin: 50px auto 0;
+          }
 
-		const { loading, errorMessage, placeholder } = this.state;
+          form {
+            display: grid;
+            grid-template-columns: 2fr 3fr;
+            grid-gap: 10px;
+          }
 
-		const loaderIcon = <FontAwesomeIcon icon={faSync} spin style={{ marginLeft: 10 }} />
+          form > button.primary {
+            grid-column: 1 / -1;
+          }
 
-		return (
-			<div className="wrapper">
-				<form onSubmit={this.handleSubmit}>
-					<h3 className="title">YouTube Analyzer</h3>
-					<select onChange={this.updatePlaceholder} ref="option" name="options">
-						<option value="playlistUrl">Playlist URL</option>
-						<option value="playlistId">Playlist ID</option>
-						<option value="channelUrl">Channel URL</option>
-						<option value="channelId">Channel ID</option>
-					</select>
-					<div>
-						<input type="text" ref="text" placeholder={placeholder} />
-						<span className="form-error is-visible" id="uuid">{errorMessage}</span>
-					</div>
-					<button className="primary button" type="submit">Submit {loading && loaderIcon}</button>
-				</form>
-				<style jsx>{`
-
-					.wrapper {
-						padding: 30px 20px;
-						max-width: 700px;
-						border: 1px solid rgba(0,0,0,.2);
-						margin: 50px auto 0;
-					}
-
-					form {
-						display: grid;
-						grid-template-columns: 2fr 3fr;
-						grid-gap: 10px;
-					}
-
-					form > button.primary {
-						grid-column: 1 / -1;
-					}
-
-					.title {
-						grid-column: 1 / -1;
-						text-align: center;
-					}
-				
-				`}</style>
-			</div>
-		)
-	}
+          .title {
+            grid-column: 1 / -1;
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    );
+  }
 }
 
 export default Form;
