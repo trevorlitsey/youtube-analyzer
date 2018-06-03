a0efdf461be8f0e6d47951c7064cd20c8bcb2d50
Trevor <litseyt@me.com>
2018-06-03 16:59:38 -0500

    fix bug in totaler to add 0 if value is undefined

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "ae64f952248d3561fc7a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5c6256ad086f1f920069"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js","4":"bundles/pages/search.js"}[chunkId]||chunkId) + "-" + {"0":"3071c82fc573461ebffd","1":"3763de13a4559be52569","3":"ac233d9f128a72b49f7c","4":"bee01d2e0805a4a551d7"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js","4":"bundles/pages/search.js"}[chunkId]||chunkId) + "-" + {"0":"3071c82fc573461ebffd","1":"3763de13a4559be52569","3":"bda4c61c411a2c66f16c","4":"cae303cd4af655a3f4da"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- helpers.js
+++ helpers.js
@@ -1,195 +1,184 @@
-const axios = require('axios');
-const iso = require('iso8601-duration')
+const axios = require("axios");
+const iso = require("iso8601-duration");
 const { parse, toSeconds } = iso;
 
-const getPlaylistIdFromUrl = (url) => {
-	const parsed = getJsonFromUrl(url);
-	return parsed.list;
-}
+const getPlaylistIdFromUrl = url => {
+  const parsed = getJsonFromUrl(url);
+  return parsed.list;
+};
 
-const getChannelIdFromUrl = (url = '') => {
-	const arr = url.split('/');
-	return arr[arr.length - 1];
-}
+const getChannelIdFromUrl = (url = "") => {
+  const arr = url.split("/");
+  return arr[arr.length - 1];
+};
 
-const getApiUrl = (section, userParams, apiKey = 'AIzaSyAlAtI54mpJ7iDblL-pisPlQr8F6vmTy0A') => {
+const getApiUrl = (
+  section,
+  userParams,
+  apiKey = "AIzaSyAlAtI54mpJ7iDblL-pisPlQr8F6vmTy0A",
+) => {
+  const params = {
+    part: "snippet",
+    maxResults: 50,
+    ...userParams,
+  };
 
-	const params = {
-		part: 'snippet',
-		maxResults: 50,
-		...userParams,
-	}
+  const baseUrl = `https://www.googleapis.com/youtube/v3/${section}?key=${apiKey}`;
 
-	const baseUrl = `https://www.googleapis.com/youtube/v3/${section}?key=${apiKey}`
+  const paramsUrl = Object.entries(params)
+    .map(([key, value]) => {
+      return `&${key}=${value}`;
+    })
+    .join("");
 
-	const paramsUrl = Object.entries(params).map(([key, value]) => {
-		return `&${key}=${value}`
-	}).join('')
-
-	return baseUrl + paramsUrl;
-
-}
+  return baseUrl + paramsUrl;
+};
 
 const getResultPages = async (apiSection, params) => {
+  const resultPages = [];
+  const firstUrl = getApiUrl(apiSection, params);
 
-	const resultPages = [];
-	const firstUrl = getApiUrl(apiSection, params)
+  // get first one
+  await axios
+    .get(firstUrl)
+    .then(res => resultPages.push(res.data))
+    .catch(err => console.error(err));
 
-	// get first one
-	await axios
-		.get(firstUrl)
-		.then(res => resultPages.push(res.data))
-		.catch(err => console.error(err));
+  if (resultPages.length) {
+    while (resultPages[resultPages.length - 1].nextPageToken) {
+      params.pageToken = resultPages[resultPages.length - 1].nextPageToken;
 
-	if (resultPages.length) {
-		while (resultPages[resultPages.length - 1].nextPageToken) {
+      const nextUrl = getApiUrl(apiSection, params);
+      const newPage = await axios
+        .get(nextUrl)
+        .then(res => res.data)
+        .catch(err => console.error(err));
+      resultPages.push(newPage);
+    }
+  }
 
-			params.pageToken = resultPages[resultPages.length - 1].nextPageToken
+  return resultPages;
+};
 
-			const nextUrl = getApiUrl(apiSection, params)
-			const newPage = await axios.get(nextUrl).then(res => res.data).catch(err => console.error(err))
-			resultPages.push(newPage);
+const getVideosFromPlaylistId = async playlistId => {
+  const params = {
+    playlistId,
+    part: "contentDetails,snippet",
+  };
 
-		}
-	}
+  const resultPages = await getResultPages("playlistItems", params);
 
-	return resultPages;
+  // video ids in [50, 50, remaining]
+  const videoIds = resultPages.map(obj =>
+    obj.items.map(item => item.contentDetails.videoId),
+  );
 
-}
+  return videoIds;
+};
 
-const getVideosFromPlaylistId = async (playlistId) => {
+const getVideosFromChannelId = async channelId => {
+  let videoIds = [];
 
-	const params = {
-		playlistId,
-		part: 'contentDetails,snippet',
-	}
+  const params = {
+    channelId,
+  };
 
-	const resultPages = await getResultPages('playlistItems', params)
+  const resultPages = await getResultPages("search", params);
 
-	// video ids in [50, 50, remaining]
-	const videoIds = resultPages
-		.map(obj =>
-			obj.items.map(item =>
-				item.contentDetails.videoId
-			))
+  if (resultPages.length) {
+    // video ids in [50, 50, remaining]
+    videoIds = resultPages.map(obj => obj.items.map(item => item.id.videoId));
+  }
 
-	return videoIds;
+  return videoIds;
+};
 
-}
+const processVideoIds = async videoIds => {
+  const videoPromises = videoIds.map(page => {
+    const id = page.join(",");
+    const url = getApiUrl("videos", {
+      part: "statistics,snippet,contentDetails",
+      id,
+    });
+    return axios.get(url);
+  });
 
-const getVideosFromChannelId = async (channelId) => {
+  const res = await Promise.all(videoPromises);
 
-	let videoIds = [];
+  const videoDetails = [];
+  res
+    .map(res => res.data)
+    .map(res => res.items)
+    .forEach(page => videoDetails.push(...page));
 
-	const params = {
-		channelId,
-	}
+  const reducedDetails = reduceDetails(videoDetails);
 
-	const resultPages = await getResultPages('search', params);
+  return reducedDetails;
 
-	if (resultPages.length) {
-		// video ids in [50, 50, remaining]
-		videoIds = resultPages
-			.map(obj =>
-				obj.items.map(item =>
-					item.id.videoId
-				))
-	}
+  //--------------
+  function reduceDetails(videoDetails) {
+    const reduced = [];
+    videoDetails.forEach(video => {
+      reduced.push({
+        id: video.id,
+        channelTitle: video.snippet.channelTitle,
+        title: video.snippet.title,
+        publishedAt: video.snippet.publishedAt,
+        viewCount: video.statistics.viewCount,
+        duration: video.contentDetails.duration,
+        commentCount: video.statistics.commentCount,
+        likeCount: video.statistics.likeCount,
+        dislikeCount: video.statistics.dislikeCount,
+      });
+    });
 
-	return videoIds;
-}
-
-const processVideoIds = async (videoIds) => {
-
-	const videoPromises = videoIds.map(page => {
-		const id = page.join(',');
-		const url = getApiUrl('videos', { part: 'statistics,snippet,contentDetails', id })
-		return axios.get(url)
-	})
-
-	const res = await Promise.all(videoPromises);
-
-	const videoDetails = [];
-	res
-		.map(res => res.data)
-		.map(res => res.items)
-		.forEach(page => videoDetails.push(...page));
-
-	const reducedDetails = reduceDetails(videoDetails)
-
-	return reducedDetails
-
-	//--------------
-	function reduceDetails(videoDetails) {
-
-		const reduced = []
-		videoDetails.forEach(video => {
-			reduced.push({
-				id: video.id,
-				channelTitle: video.snippet.channelTitle,
-				title: video.snippet.title,
-				publishedAt: video.snippet.publishedAt,
-				viewCount: video.statistics.viewCount,
-				duration: video.contentDetails.duration,
-				commentCount: video.statistics.commentCount,
-				likeCount: video.statistics.likeCount,
-				dislikeCount: video.statistics.dislikeCount,
-			})
-		})
-
-		return reduced;
-
-	}
-
-}
-
+    return reduced;
+  }
+};
 
 export function getVideoTotals(videoDetails: Array<any>) {
-	const statistics = {
-		viewCount: 0,
-		commentCount: 0,
-		likeCount: 0,
-		dislikeCount: 0,
-		duration: 0,
-		totalVideos: videoDetails.length,
-	}
+  const statistics = {
+    viewCount: 0,
+    commentCount: 0,
+    likeCount: 0,
+    dislikeCount: 0,
+    duration: 0,
+    totalVideos: videoDetails.length,
+  };
 
-	videoDetails.forEach((video) => {
-		Object.entries(video).forEach(([key, value]) => {
-			statistics[key] = statistics[key] + Number(value);
-		})
-	})
+  videoDetails.forEach(video => {
+    Object.entries(video).forEach(([key, value]) => {
+      statistics[key] = statistics[key] + Number(value) || 0;
+    });
+  });
 
-	statistics.duration = videoDetails.reduce((acc, video) =>
-		acc += toSeconds(parse(video.duration)), 0)
+  statistics.duration = videoDetails.reduce(
+    (acc, video) => (acc += toSeconds(parse(video.duration))),
+    0,
+  );
 
-	return statistics;
+  return statistics;
 }
 
 const helpers = {
-	getPlaylistIdFromUrl,
-	getChannelIdFromUrl,
-	getApiUrl,
-	getVideosFromChannelId,
-	getVideosFromPlaylistId,
-	processVideoIds,
-	getVideoTotals,
-}
+  getPlaylistIdFromUrl,
+  getChannelIdFromUrl,
+  getApiUrl,
+  getVideosFromChannelId,
+  getVideosFromPlaylistId,
+  processVideoIds,
+  getVideoTotals,
+};
 
 module.exports = helpers;
 
-
-
-
-
-
 // ********
 function getJsonFromUrl(url) {
-	var query = url.split('?')[1];
-	var result = {};
-	query.split("&").forEach(function (part) {
-		var item = part.split("=");
-		result[item[0]] = decodeURIComponent(item[1]);
-	});
-	return result;
+  var query = url.split("?")[1];
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
 }
